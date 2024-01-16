c56a995ed8c162de5f65b4eb0d09449bf8206739
Lyove <lyove@outlook.com>
2024-01-16 21:28:13 +0800

    init

--- resources/views/vendor/laravel-menu/bootstrap-navbar-items.blade.php
+++ resources/views/vendor/laravel-menu/bootstrap-navbar-items.blade.php
@@ -9,8 +9,7 @@
     @endif
     @if($item->hasChildren())
       <ul class="dropdown-menu">
-        @include(config('laravel-menu.views.bootstrap-items'),
-array('items' => $item->children()))
+        @include(config('laravel-menu.views.bootstrap-items'), array('items' => $item->children()))
       </ul>
     @endif
   </li>
