c4582627b377b1374e73268eecf9bd6ca504261e
Lyove <lyove@outlook.com>
2024-01-17 17:05:09 +0800

    init

--- resources/views/frontend/includes/meta.blade.php
+++ resources/views/frontend/includes/meta.blade.php
@@ -44,6 +44,7 @@ $meta_page_type = 'website';
 <meta property="og:image:height" content="630" />
 
 <!-- Twitter Meta -->
+<!-- 
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="{{ setting('meta_twitter_site') }}">
 <meta name="twitter:url" content="{{url()->full()}}" />
@@ -51,7 +52,8 @@ $meta_page_type = 'website';
 <meta name="twitter:title" content="@yield('title') | {{ config('app.name') }}">
 <meta name="twitter:description" content="{{ setting('meta_description') }}">
 <meta name="twitter:image" content="{{ asset(setting('meta_image')) }}">
+-->
 
 <!--canonical link-->
-<meta name="generator" content="Laravel Starter - A modular CMS starter  application built with Laravel 10.x." />
+<meta name="generator" content="Laravel Starter - A modular CMS starter application built with Laravel 10.x." />
 <link rel="canonical" href="{{url()->full()}}">
