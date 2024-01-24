ea7a79d6e3c3ec3dd5466343ede7179b11c0ef8b
Lyove <lyove@outlook.com>
2024-01-25 00:33:22 +0800

    init

--- resources/views/components/frontend/card.blade.php
+++ resources/views/components/frontend/card.blade.php
@@ -1,6 +1,6 @@
 @props(['url', 'name', 'image'=>''])
 
-<div class="flex flex-col bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<div class="flex flex-col bg-white border border-gray-200 rounded-lg shadow hover:shadow-lg dark:bg-gray-800 dark:border-gray-700">
     @if ($image)
     <div class="rounded-t-lg overflow-hidden">
         <a href="{{$url}}">
