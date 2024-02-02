14e24329ca809f93b62d0d3a77f7767518a6287f
Lyove <lyove@outlook.com>
2024-02-02 19:42:21 +0800

    init

--- app/Http/Controllers/Backend/BackendBaseController.php
+++ app/Http/Controllers/Backend/BackendBaseController.php
@@ -194,6 +194,10 @@ class BackendBaseController extends Controller
 
         $module_action = 'Store';
 
+        if ($request->input('slug')) {
+            $request['slug'] = slugify($request->input('slug'));
+        }
+
         $$module_name_singular = $module_model::create($request->all());
 
         flash(icon()."New '".Str::singular($module_title)."' Added")->success()->important();
@@ -281,6 +285,10 @@ class BackendBaseController extends Controller
         $module_action = 'Update';
 
         $$module_name_singular = $module_model::findOrFail($id);
+
+        if ($request->input('slug')) {
+            $request['slug'] = slugify($request->input('slug'));
+        }
 
         $$module_name_singular->update($request->all());
 
