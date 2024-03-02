c3670f0dddcc5851141c606f923606712a2672f6
Lyove <lyove@outlook.com>
2024-03-02 16:18:41 +0800

    init

--- resources/views/components/frontend/card.blade.php
+++ resources/views/components/frontend/card.blade.php
@@ -20,7 +20,7 @@
     </div>
     <div class="px-5 text-end pb-5">
         <a href="{{$url}}" class="inline-flex items-center text-sm outline outline-1 outline-gray-800 text-gray-700 hover:text-gray-100 bg-gray-200 hover:bg-gray-700 py-2 px-3 focus:outline-none rounded">
-            View details
+            {{__("View details")}}
             <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
             </svg>
