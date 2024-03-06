b733f4d23627c846a9fa681dfeac5de9e371fc3a
Lyove <lyove@outlook.com>
2024-03-06 17:59:20 +0800

    init

--- modules/Page/Resources/views/backend/pages/form.blade.php
+++ modules/Page/Resources/views/backend/pages/form.blade.php
@@ -31,9 +31,9 @@
             $field_placeholder = "-- Select an option --";
             $required = "required";
             $select_options = [
-                '1'=>'Published',
-                '0'=>'Unpublished',
-                '2'=>'Draft'
+                '1'=>__('Published'),
+                '0'=>__('Unpublished'),
+                '2'=>__('Draft')
             ];
             ?>
             {{ html()->label($field_label, $field_name)->class('form-label') }} {!! fielf_required($required) !!}
