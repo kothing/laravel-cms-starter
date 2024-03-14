d558902b326d30da7a0cd8f3c264567151609986
Lyove <lyove@outlook.com>
2024-03-14 11:50:44 +0800

    init

--- resources/views/frontend/users/changePassword.blade.php
+++ resources/views/frontend/users/changePassword.blade.php
@@ -21,7 +21,7 @@
 
                 <div class="pt-4 text-center">
                     <a href='{{ route("frontend.users.profile", encode_id($$module_name_singular->id)) }}'>
-                        <div class="w-full font-semibold text-sm px-6 py-2 transition ease-in duration-200 rounded text-gray-500 hover:bg-gray-800 hover:text-white border-1 border-gray-900 focus:outline-none">
+                        <div class="w-full font-semibold text-sm px-6 py-2 outline transition ease-in duration-200 rounded text-gray-500 hover:bg-gray-800 hover:text-white border-1 border-gray-900 focus:outline-none">
                             @lang(' View Profile')
                         </div>
                     </a>
@@ -30,7 +30,7 @@
         </div>
         <div class="mt-5 md:mt-0 sm:col-span-2">
             {{ html()->form('PATCH', route('frontend.users.changePasswordUpdate', encode_id($$module_name_singular->id)))->class('form-horizontal')->open() }}
-            <div class="mb-8 p-6 bg-white border shadow-lg rounded-lg">
+            <div class="mb-8 p-6 bg-white border rounded-lg">
                 <div class="grid grid-cols-6 gap-6">
                     <div class="col-span-6 sm:col-span-3">
                         <?php
@@ -52,7 +52,7 @@
                         {{ html()->label($field_label, $field_name)->class('block-inline text-sm font-medium text-gray-700') }} {!! fielf_required($required) !!}
                         {{ html()->password($field_name)->placeholder($field_placeholder)->class('mt-1 border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-300 rounded border shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent')->attributes(["$required"]) }}
                     </div>
-                    <div class="col-span-6 px-4 py-3 bg-gray-50 text-end sm:px-6">
+                    <div class="col-span-6 px-4 py-3 text-end sm:px-6">
                         <button type="submit" class="inline-flex w-full justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                             @lang('Update Password')
                         </button>
@@ -75,11 +75,11 @@
                 </div>
             </div>
             <div class="mt-5 md:mt-0 sm:col-span-2">
-                <div class="mb-8 p-6 bg-white border shadow-lg rounded-lg">
+                <div class="mb-8 p-6 bg-white border rounded-lg">
                     <div class="grid grid-cols-6 gap-6">
                         <div class="col-span-6 text-center">
                             <a href="{{ route('frontend.users.profileEdit', encode_id($$module_name_singular->id)) }}">
-                                <div class="w-full font-semibold text-sm px-6 py-2 transition ease-in duration-200 rounded text-gray-500 hover:bg-gray-800 hover:text-white border-1 border-gray-900 focus:outline-none">
+                                <div class="w-full font-semibold text-sm px-6 py-2 outline transition ease-in duration-200 rounded text-gray-500 hover:bg-gray-800 hover:text-white border-1 border-gray-900 focus:outline-none">
                                     Edit Profile
                                 </div>
                             </a>
--- resources/views/frontend/users/profileEdit.blade.php
+++ resources/views/frontend/users/profileEdit.blade.php
@@ -29,7 +29,7 @@
         </div>
         <div class="mt-5 sm:mt-0 sm:col-span-2">
             {{ html()->modelForm($userprofile, 'PATCH', route('frontend.users.profileUpdate', encode_id($$module_name_singular->id)))->acceptsFiles()->open() }}
-            <div class="mb-8 p-6 bg-white border shadow-lg rounded-lg">
+            <div class="mb-8 p-6 bg-white border rounded-lg">
                 <div class="grid grid-cols-6 gap-6">
 
                     <div class="col-span-6 sm:col-span-3">
@@ -75,61 +75,6 @@
                         {{ html()->label($field_label, $field_name)->class('block-inline text-sm font-medium text-gray-700') }} {!! fielf_required($required) !!}
                         {{ html()->text($field_name)->placeholder($field_placeholder)->class('mt-1 border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-300 rounded border shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent')->attributes(["$required"]) }}
                     </div>
-
-                    <div class="col-span-6 sm:col-span-3">
-                        <?php
-                        $field_name = 'url_facebook';
-                        $field_label = label_case($field_name);
-                        $field_placeholder = $field_label;
-                        $required = "";
-                        ?>
-                        {{ html()->label($field_label, $field_name)->class('block-inline text-sm font-medium text-gray-700') }} {!! fielf_required($required) !!}
-                        {{ html()->text($field_name)->placeholder($field_placeholder)->class('mt-1 border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-300 rounded border shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent')->attributes(["$required"]) }}
-                    </div>
-
-                    <div class="col-span-6 sm:col-span-3">
-                        <?php
-                        $field_name = 'url_twitter';
-                        $field_label = label_case($field_name);
-                        $field_placeholder = $field_label;
-                        $required = "";
-                        ?>
-                        {{ html()->label($field_label, $field_name)->class('block-inline text-sm font-medium text-gray-700') }} {!! fielf_required($required) !!}
-                        {{ html()->text($field_name)->placeholder($field_placeholder)->class('mt-1 border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-300 rounded border shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent')->attributes(["$required"]) }}
-                    </div>
-
-                    <div class="col-span-6 sm:col-span-3">
-                        <?php
-                        $field_name = 'url_linkedin';
-                        $field_label = label_case($field_name);
-                        $field_placeholder = $field_label;
-                        $required = "";
-                        ?>
-                        {{ html()->label($field_label, $field_name)->class('block-inline text-sm font-medium text-gray-700') }} {!! fielf_required($required) !!}
-                        {{ html()->text($field_name)->placeholder($field_placeholder)->class('mt-1 border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-300 rounded border shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent')->attributes(["$required"]) }}
-                    </div>
-
-                    <div class="col-span-6 sm:col-span-3">
-                        <?php
-                        $field_name = 'url_instagram';
-                        $field_label = label_case($field_name);
-                        $field_placeholder = $field_label;
-                        $required = "";
-                        ?>
-                        {{ html()->label($field_label, $field_name)->class('block-inline text-sm font-medium text-gray-700') }} {!! fielf_required($required) !!}
-                        {{ html()->text($field_name)->placeholder($field_placeholder)->class('mt-1 border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-300 rounded border shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent')->attributes(["$required"]) }}
-                    </div>
-                    <div class="col-span-6">
-                        <?php
-                        $field_name = 'bio';
-                        $field_label = label_case($field_name);
-                        $field_placeholder = $field_label;
-                        $required = "";
-                        ?>
-                        {{ html()->label($field_label, $field_name)->class('block-inline text-sm font-medium text-gray-700') }} {!! fielf_required($required) !!}
-                        {{ html()->textarea($field_name)->placeholder($field_placeholder)->class('mt-1 border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-300 rounded border shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent')->attributes(["$required", 'rows'=> 5]) }}
-                    </div>
-
                 </div>
                 <div class="grid grid-cols-6 gap-6 mt-4">
 
@@ -172,7 +117,7 @@
             </div>
         </div>
         <div class="mt-5 sm:mt-0 sm:col-span-2">
-            <div class="mb-8 p-6 bg-white border shadow-lg rounded-lg">
+            <div class="mb-8 p-6 bg-white border rounded-lg">
                 <div class="grid grid-cols-6 gap-6">
                     <div class="col-span-6">
                         <label class="block-inline text-sm font-medium text-gray-700" for="first_name">Email</label> <span class="text-danger text-red-600">*</span>
@@ -218,7 +163,7 @@
                         {{ html()->select($field_name, $select_options)->placeholder($field_placeholder)->class('mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm')->attributes(["$required"]) }}
                     </div>
                 </div>
-                <div class="mt-4 px-4 bg-gray-50 text-end sm:px-6">
+                <div class="mt-4 px-4 text-end sm:px-6">
                     <button type="submit" class="inline-flex w-full justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                         Save
                     </button>
@@ -246,7 +191,7 @@
                 </div>
             </div>
             <div class="mt-5 sm:mt-0 sm:col-span-2">
-                <div class="mb-8 p-6 bg-white border shadow-lg rounded-lg">
+                <div class="mb-8 p-6 bg-white border rounded-lg">
                     <div class="grid grid-cols-6 gap-6">
                         <div class="col-span-6 text-center">
                             <a href="{{ route('frontend.users.changePassword', encode_id($$module_name_singular->id)) }}">
