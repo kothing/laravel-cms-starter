a08525c7c477a649d5b0918b8d3f1ba50d358af5
Lyove <lyove@outlook.com>
2024-03-15 17:25:45 +0800

    init

--- resources/assets/js/app-frontend.js
+++ resources/assets/js/app-frontend.js
@@ -1,7 +1,16 @@
 import axios from "axios";
+import * as coreui from "@coreui/coreui";
 import { Livewire } from "~vendor/livewire/livewire/dist/livewire.esm";
 
 window.axios = axios;
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 Livewire.start();
+
+/**
+ * Enable tooltips everywhere
+ */
+(function ininTooltip() {
+    const tooltipTriggerList = document.querySelectorAll('[data-toggle="tooltip"]') || [];
+    [...tooltipTriggerList].map((tooltipTriggerEl) => new coreui.Tooltip(tooltipTriggerEl));
+})();
