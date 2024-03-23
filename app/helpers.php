8cd58a57b832964ff235e3be30e3e6adacbc9b44
Lyove <lyove@outlook.com>
2024-03-23 21:56:40 +0800

    init

--- app/helpers.php
+++ app/helpers.php
@@ -265,7 +265,7 @@ if (! function_exists('icon')) {
      */
     function icon($string = 'fa-regular fa-circle-check')
     {
-        return "<i class='".$string."'></i>;
+        return "<i class='".$string."'></i>&nbsp;";
     }
 }
 
